d852532836cd4a19573d2d2b69e3e995708ca420
OmarHSalim <Omar.Salim.eng@gmail.com>
2024-02-26 11:48:06 +1100

    everything updated

--- template.py
+++ template.py
@@ -1,48 +1,35 @@
 import os
 from pathlib import Path
 
-#package_name = "mongodb_connect"
+package_name = "mongodb_connect"
 
 list_of_files = [
-   ".github/workflows/.gitkeep", # CI/CD, Folder must have a file to be visiable when push to Github
-   # src: source code
+   ".github/workflows/ci.yaml",
    "src/__init__.py",
-   # This for compenents has pipelines such as data cleaning, feature engineering, data preperations
-   # Each operation is pipelines  
-   "src/compenents/__init__.py", # ipnv file
-   "src/compenents/data_integration.py",
-   "src/compenents/data_transformation.py",
-   "src/compenents/model_trainer.py",
-   "src/compenents/model_evaluation.py", 
-   "src/pipleline/__init__.py",
-   "src/pipleline/training_pipeline.py",  # This is training pipeline
-   "src/pipleline/prediction_pipeline.py", # This is prediction or inferencing pipeline
-   "src/utils/__init__.py",
-   "src/utils/utils.py", # write all the utility method
-   "src/logger/logging.py",
-   "src/exception/exception.py",
-   "src/unit/__init__.py",
-   "tests/unit/__init__.py", # integration
-   "tests/integration/__init__.py", # integration
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/unit.py",
+   "tests/integration/__init__.py",
+   "tests/integration/int.py", # test cases
    "init_setup.sh",
-   "requirements.txt", # development environment only
-   "requirements_dev.txt", # development environment only 
+   "requirements.txt", # production env
+   "requirements_dev.txt", 
    "setup.py",
    "setup.cfg",
    "pyproject.toml",
    "tox.ini",
-   "experiments/experiments.ipynb", # perform all experiments inside ipynb file
+   "experiments/experiments.ipynb", 
 ]
 
 for filepath in list_of_files:
-    filepath = Path(filepath) # get filepath that compitable with the system
-    filedir, filename = os.path.split(filepath) # exp. in line 33, filedir = experiments, filename = experiments.ipynb
-    if filedir != "": # not empty
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0): # size of the empty file = 0
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create an empty file
-
 
 #its updated
---
+++ tests/integration/int.py
@@ -0,0 +1,3 @@
+def test_empty():
+    assert True # These two lines are added to avoid to make the file empty 
+    
